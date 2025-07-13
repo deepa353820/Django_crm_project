aaea5fc073c91eaddb2b84617c8f0be7800348db
deepa353820 <122205019+deepa353820@users.noreply.github.com>
2025-07-13 14:38:37 +0530

    Delete pandastest directory

--- pandastest/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class PandastestConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'pandastest'
--- pandastest/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-# from . import views
-
-urlpatterns = [
-
-    # path('demo',views.employees , name='demo'),
-    # path('tests',views.test_data,name='tests')
-
-]
